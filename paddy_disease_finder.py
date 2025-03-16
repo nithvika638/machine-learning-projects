8bad23cae41a4b55d3e4d6059a4204dc2c04af48
nithvika638 <nithvika2125@gmail.com>
2025-03-16 23:48:38 +0530

    Update paddy_disease_finder.py

--- paddy_disease_finder.py
+++ paddy_disease_finder.py
@@ -7,7 +7,7 @@ import numpy as np
 from PIL import Image
 st.title("paddy crop disease detection ")
 st.write("Upload an image of rice to detect the disease ")
-MODEL_PATH = "C:\\Users\\karur\\Downloads\\vgg16_rice_model.h5"
+MODEL_PATH = r"C:\Users\YourUsername\Downloads\my_model.h5"
 if os.path.exists(MODEL_PATH):
     model = load_model(MODEL_PATH)
 classes = ['Bacterialblight', 'Blast', 'Brownspot', 'Tungro']
